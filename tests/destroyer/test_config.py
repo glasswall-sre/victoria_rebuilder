4f5bced1d4e0ba71d0bfb5463df4c9bd58be0512
Alexander Potter-Dixon <apotter-dixon@gw.local>
2019-12-08 15:08:02 +0000

    Initial tests added

--- tests/destroyer/test_config.py
+++ tests/destroyer/test_config.py
@@ -1,16 +1,15 @@
 import pytest
 
-from config import StageConfig, StageSchema
-
-
+from destroyer.config import StageConfig, StageSchema
 
 
 def test_create_stage_config():
-  
+
     result = StageSchema().load({
         "pipelines": ["Platform.test", "Platform.test2"],
-        "dependsOn": "basic_infra"
-       
+        "dependsOn":
+        "basic_infra"
     })
 
-    assert result == StageConfig(["Platform.test", "Platform.test2"],"basic_infra")
+    assert result == StageConfig(["Platform.test", "Platform.test2"],
+                                 "basic_infra")
